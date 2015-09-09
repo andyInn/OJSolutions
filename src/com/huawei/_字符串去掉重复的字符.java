f9c025dc2714f03faf07ab8e7dd857808e8c0597
andyInn <saint.hanzq@gmail.com>
2015-09-09 19:36:46 +0800

    huawei practise
    practise

---
+++ src/com/huawei/_字符串去掉重复的字符.java
@@ -0,0 +1,25 @@
+package com.huawei;
+
+import java.util.Scanner;
+
+public class _字符串去掉重复的字符 {
+	
+	public static String removeDuplicates(String s){
+		if(s==null)
+			return "";
+		if(s.length()<2)
+			return s;
+		StringBuilder sb=new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			if(sb.indexOf(String.valueOf(s.charAt(i)))==-1)
+				sb.append(s.charAt(i));
+		}
+		return sb.toString();
+	}
+
+	public static void main(String ...strings){
+//		Scanner cin=new Scanner(System.in);
+//		String s=cin.next();
+		System.out.println(removeDuplicates("111235223568"));
+	}
+}
