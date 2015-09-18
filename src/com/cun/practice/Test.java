33258868a88c9a3cba0e8e68cbad519c478a5b8f
andyInn <saint.hanzq@gmail.com>
2015-09-18 11:00:48 +0800

    Java base problem
    memo

---
+++ src/com/cun/practice/Test.java
@@ -0,0 +1,15 @@
+package com.cun.practice;
+
+public class Test{
+	public static void main(String ...strings){
+		int i=1;
+		byte b=1;
+		char c=97;
+		short s=1;
+		System.out.println("i:"+i+",b:"+b+",c:"+c+",s:"+s);
+		byte bb=127;
+		char cc=65535;
+		short ss=32767;
+		System.out.println("bb:"+bb+",cc:"+cc+",ss:"+ss);
+	}
+}
