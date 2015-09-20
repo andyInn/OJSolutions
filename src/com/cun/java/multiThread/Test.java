94266b05c9e9fa8230b6d6aa3187f3a136b8c45a
andyInn <saint.hanzq@gmail.com>
2015-09-20 22:34:33 +0800

    Multi-Thread practise
    Insert into a new queue in turns using two threads from a source queue,
    and make sure that the order is right.

---
+++ src/com/cun/java/multiThread/Test.java
@@ -0,0 +1,80 @@
+package com.cun.java.multiThread;
+
+import java.util.LinkedList;
+import java.util.Queue;
+/**
+ * cvte面试中的一道题求解，队列由两个线程取出处理，然后按照原来的队列顺序放入新队列中。
+ * 此方法可以插入新队列并打印出结果
+ * @author Andy
+ *
+ */
+public class Test{
+	public static int state=1;
+	public Queue<Integer> q;
+	public static Queue<Integer> qNew = new LinkedList<Integer>();
+	
+	public Test(Queue<Integer> q){
+		this.q=q;
+	}
+	
+	public static void main(String ...strings){
+		Queue<Integer> q=new LinkedList<Integer>();
+		q.offer(1);
+		q.offer(2);
+		q.offer(3);
+		q.offer(4);
+		q.offer(5);
+		final Test t=new Test(q);
+		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(t.q.peek()!=null){
+					synchronized(t){
+						while(state==2){
+							try {
+								t.wait();
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+						if(t.q.peek()!=null){
+							System.out.println("thread:1");
+							qNew.offer(t.q.poll());
+						}
+						state=2;
+						t.notifyAll();
+					}
+				}
+			}
+		}).start();
+		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(qNew.peek()!=null){
+					synchronized(t){
+						while(state==1){
+							try {
+								t.wait();
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+						if(t.q.peek()!=null){
+							System.out.println("thread:2");
+							qNew.offer(t.q.poll());
+						}
+						state=1;
+						t.notifyAll();
+					}
+				}
+			}
+		}).start();
+		while(!t.q.isEmpty());//wait till q is empty
+		int i = 0;
+		while (qNew.peek() != null) {
+			System.out.println(i++ +": " + qNew.poll());
+		}
+	}
+}
---
+++ src/com/cun/java/multiThread/Test2.java
@@ -0,0 +1,110 @@
+package com.cun.java.multiThread;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * cvte面试中的一道题求解，队列由两个线程取出处理，然后按照原来的队列顺序放入新队列中
+ * 此方法可以插入新队列并打印出结果
+ * @author Andy
+ *
+ */
+public class Test2{
+	public static void main(String ...strings){
+		Queue<Integer> q=new LinkedList<Integer>();
+		Queue<Integer> qNew = new LinkedList<Integer>();
+		q.offer(1);
+		q.offer(2);
+		q.offer(3);
+		q.offer(4);
+		q.offer(5);
+		TwoThread tt=new TwoThread();
+		tt.q=q;
+		Thread1 t1=new Thread1(tt);
+		Thread2 t2=new Thread2(tt);
+		ExecutorService exe=Executors.newCachedThreadPool();
+		exe.execute(t1);
+		exe.execute(t2);
+		exe.shutdown();
+//		try {
+//			TimeUnit.SECONDS.sleep(2);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+		while(!exe.isTerminated());
+		qNew=TwoThread.qNew;
+		int i = 0;
+		while (qNew.peek() != null) {
+			System.out.println(i++ +": " + qNew.poll());
+		}
+	}
+}
+
+class TwoThread {
+	private static boolean flag = true;
+	public Queue<Integer> q;
+	public static Queue<Integer> qNew = new LinkedList<Integer>();
+	
+	public synchronized void pushOdd(){
+		while(!flag){
+			try {
+				wait();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (q.peek() != null) {
+			System.out.println("thread: Odd-"+q.peek());
+			qNew.offer(q.poll());
+		}
+		flag=false;
+		notify();
+	}
+	
+	public synchronized void pushEven(){
+		while(flag){
+			try {
+				wait();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (q.peek() != null) {
+			System.out.println("thread: Even-"+q.peek());
+			qNew.offer(q.poll());
+		}
+		flag=true;
+		notify();
+	}
+}
+
+class Thread1 implements Runnable {
+	private TwoThread tt;
+	
+	public Thread1(TwoThread tt){
+		this.tt=tt;
+	}
+	@Override
+	public void run() {
+		while (tt.q.peek() != null) {
+			tt.pushOdd();
+		}
+	}
+}
+
+class Thread2 implements Runnable {
+	private TwoThread tt;
+	
+	public Thread2(TwoThread tt){
+		this.tt=tt;
+	}
+	@Override
+	public void run() {
+		while (tt.q.peek() != null) {
+			tt.pushEven();
+		}
+	}
+}
