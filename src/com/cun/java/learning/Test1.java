47d9445179560086b29a22c198fa35abd74969d6
andyInn <saint.hanzq@gmail.com>
2015-09-21 21:09:12 +0800

    daily update
    practise

--- src/com/cun/java/learning/Test1.java
+++ src/com/cun/java/learning/Test1.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public abstract class Test1 {
 	public static void main(String[] args) {
-		int[] A=new int[10];
+		Scanner cin=new Scanner(System.in);
+		String line=cin.nextLine();
+		if(line.length()==0||line.equals(""))
+			System.out.println("当前为空行");
+/*		int[] A=new int[10];
 		int[][] A2=new int[10][5];
 		List<Integer> list=new ArrayList<Integer>();
 		Map<Integer, Integer> map=new HashMap<Integer, Integer>();
 		System.out.println(A2.length);
-		System.out.println(A2[1].length);
+		System.out.println(A2[1].length);*/
 //		System.out.println(A[5]);
 //		System.out.println(A2[5][6]);
 //		System.out.println(list.size());
