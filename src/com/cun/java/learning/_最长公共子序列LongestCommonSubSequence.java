6ca859fe9a709bda2c5da73f84aab40f9bb6ab2b
andyInn <saint.hanzq@gmail.com>
2015-10-05 23:52:04 +0800

    small changes
    update

--- src/com/cun/java/learning/_最长公共子序列LongestCommonSubSequence.java
+++ src/com/cun/java/learning/_最长公共子序列LongestCommonSubSequence.java
@@ -5,18 +5,15 @@ import java.util.List;
 import java.util.Random;
 
 public class _最长公共子序列LongestCommonSubSequence {
-	public static List<Character> getLCSSequence(String x,String y){
-        int len1 = x.length();
-        int len2 = y.length(); // 具体大小可自行设置
- 
+	public static List<Character> getLCSSequence(String s1,String s2){
         // 构造二维数组记录子问题x[i]和y[i]的LCS的长度
-        int[][] opt = new int[len1 + 1][len2 + 1];
+        int[][] opt = new int[s1.length() + 1][s2.length() + 1];
         List<Character> res=new ArrayList<Character>();
  
         // 从后向前，动态规划计算所有子问题,也可从前到后
-        for (int i = len1 - 1; i >= 0; i--) {
-            for (int j = len2 - 1; j >= 0; j--) {
-                if (x.charAt(i) == y.charAt(j))// 状态转移方程，现在的情况是左上角元素等于右下角元素值加1
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            for (int j = s2.length() - 1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j))// 状态转移方程，现在的情况是左上角元素等于右下角元素值加1
                     opt[i][j] = opt[i + 1][j + 1] + 1;
                 else// 状态转移方程，因为是子序列，故字母可以不挨着
                     opt[i][j] = Math.max(opt[i + 1][j], opt[i][j + 1]);
@@ -24,9 +21,9 @@ public class _最长公共子序列LongestCommonSubSequence {
         }
  
         int i = 0, j = 0;
-        while (i < len1 && j < len2) {
-            if (x.charAt(i) == y.charAt(j)) {
-            	res.add(x.charAt(i));
+        while (i < s1.length() && j < s2.length()) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+            	res.add(s1.charAt(i));
                 i++;
                 j++;
             } else if (opt[i + 1][j] >= opt[i][j + 1])
