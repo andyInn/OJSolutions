25c52f15cb001830c50fc28dd70f14a967b21c2a
andyInn <saint.hanzq@gmail.com>
2015-10-16 16:23:05 +0800

    modified errors
    daily update.
    Syncronized  multi-thread on anonymous threads.

--- src/com/cun/java/multiThread/_严格交替打印数据.java
+++ src/com/cun/java/multiThread/_严格交替打印数据.java
@@ -10,40 +10,39 @@ import java.util.Queue;
  */
 public class _严格交替打印数据{
 	public static int state=1;
-	public Queue<Integer> q;
+	public static Queue<Integer> qSource = new LinkedList<Integer>();
 	public static Queue<Integer> qNew = new LinkedList<Integer>();
 	
-	public _严格交替打印数据(Queue<Integer> q){
-		this.q=q;
+	public _严格交替打印数据(){
+		qSource.offer(1);
+		qSource.offer(2);
+		qSource.offer(3);
+		qSource.offer(4);
+		qSource.offer(5);
 	}
 	
 	public static void main(String ...strings){
-		Queue<Integer> q=new LinkedList<Integer>();
-		q.offer(1);
-		q.offer(2);
-		q.offer(3);
-		q.offer(4);
-		q.offer(5);
-		final _严格交替打印数据 t=new _严格交替打印数据(q);
+		final _严格交替打印数据 t=new _严格交替打印数据();
+		final Object lock=new Object();
 		
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				while(t.q.peek()!=null){
-					synchronized(t){
+				while(qSource.peek()!=null){
+					synchronized(lock){
 						while(state==2){
 							try {
-								t.wait();
+								lock.wait();
 							} catch (Exception e) {
 								e.printStackTrace();
 							}
 						}
-						if(t.q.peek()!=null){
+						if(qSource.peek()!=null){
 							System.out.println("thread:1");
-							qNew.offer(t.q.poll());
+							qNew.offer(qSource.poll());
 						}
 						state=2;
-						t.notifyAll();
+						lock.notifyAll();
 					}
 				}
 			}
@@ -52,26 +51,26 @@ public class _严格交替打印数据{
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				while(qNew.peek()!=null){
-					synchronized(t){
+				while(qSource.peek()!=null){
+					synchronized(lock){
 						while(state==1){
 							try {
-								t.wait();
+								lock.wait();
 							} catch (Exception e) {
 								e.printStackTrace();
 							}
 						}
-						if(t.q.peek()!=null){
+						if(qSource.peek()!=null){
 							System.out.println("thread:2");
-							qNew.offer(t.q.poll());
+							qNew.offer(qSource.poll());
 						}
 						state=1;
-						t.notifyAll();
+						lock.notifyAll();
 					}
 				}
 			}
 		}).start();
-		while(!t.q.isEmpty());//wait till q is empty
+		while(!qSource.isEmpty());//wait till qSource is empty
 		int i = 0;
 		while (qNew.peek() != null) {
 			System.out.println(i++ +": " + qNew.poll());
