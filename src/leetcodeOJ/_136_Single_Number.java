7595b50e79dc57798a580c110dcf3b0c01d062ba
andyInn <saint.hanzq@gmail.com>
2019-06-04 19:48:14 +0800

    leetcode daily update

---
+++ src/leetcodeOJ/_136_Single_Number.java
@@ -0,0 +1,8 @@
+package leetcodeOJ;
+
+public class _136_Single_Number {
+    public int singleNumber(int[] nums) {
+        for(int i=1;i<nums.length;i++) nums[0]^=nums[i];
+        return nums[0];
+    }
+}
