ece8d6b6cf130100e6e0d394006e99908620f509
andyInn <saint.hanzq@gmail.com>
2019-06-11 21:37:04 +0800

    leetcode daily update

---
+++ src/leetcodeOJ/_27_Remove_Element.java
@@ -0,0 +1,33 @@
+package leetcodeOJ;
+
+public class _27_Remove_Element {
+    /**
+     * 复杂度分析
+     *
+     * 时间复杂度：O(n)O(n)，ii 和 nn 最多遍历 nn 步。在这个方法中，赋值操作的次数等于要删除的元素的数量。因此，如果要移除的元素很少，效率会更高。
+     *
+     * 空间复杂度：O(1)O(1)。
+     *
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/two-sum/solution/yi-chu-yuan-su-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                // reduce array size by one
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+}
